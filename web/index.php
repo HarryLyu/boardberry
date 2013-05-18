284334fe65f4f34df9c49f84664b63c88894d4c5
Vitkovskii Vladimir <vitkovskii@gmail.com>
2013-05-18 12:51:53 +0400

    layout added

--- web/index.php
+++ web/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'Hello!';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/views',
+));
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('layout.twig');
+});
+
+$app->run();
