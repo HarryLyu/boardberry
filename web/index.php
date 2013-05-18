a9ae3736e1a0df56b92a452370acb0094e11374b
Vitkovskii Vladimir <vitkovskii@gmail.com>
2013-05-18 13:23:15 +0400

    debug on

--- web/index.php
+++ web/index.php
@@ -5,9 +5,10 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = 1;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/views',
+    'twig.path' => __DIR__ . '/../views',
 ));
 
 $app->get('/', function () use ($app) {
