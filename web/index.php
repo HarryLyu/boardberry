eef63fdf542edfdf9f9986aaed35c22dc0a4fd0c
criol <Yanzorik1>
2013-05-18 12:18:08 +0400

    add jquery and jquerymobile

--- web/index.php
+++ web/index.php
@@ -9,8 +9,14 @@
 
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
+
 </head>
 <body>
+
+
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
 
 </body>
 </html>
