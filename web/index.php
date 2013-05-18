13167c7cb0d1c577a672a205d9ae49368ff0dd22
SuperStas <php.progger@gmail.com>
2013-05-18 16:17:52 +0400

    Redis OK!

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/config.php';
 
 $app = new Silex\Application();
 $app['debug'] = 1;
