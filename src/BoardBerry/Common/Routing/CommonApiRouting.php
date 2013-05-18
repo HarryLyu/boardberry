4614c77b92331823e48b9bfda2ee56cafe08d62e
SuperStas <php.progger@gmail.com>
2013-05-18 17:51:12 +0400

    redis reg, user get

--- src/BoardBerry/Common/Routing/CommonApiRouting.php
+++ src/BoardBerry/Common/Routing/CommonApiRouting.php
@@ -5,6 +5,7 @@ use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommonApiRouting implements ControllerProviderInterface
 {
@@ -20,8 +21,49 @@ class CommonApiRouting implements ControllerProviderInterface
         /** @var ControllerCollection $collection */
         $collection = $app['controllers_factory'];
 
-        $collection->match('/user', function () use ($app) {
-            return new JsonResponse(['error' => 'ok']);
+        $collection->match('/user', function (Request $request) use ($app) {
+                $auth = $request->get('auth');
+                $user = $request->get('user');
+
+                if(isset($user['error'])) {
+                    throw new \Exception('error fb');
+                }
+
+                $userData = $app['user']->register($auth, $user);
+                if($userData) {
+                    $result = [
+                        'result'=>'ok',
+                        'user'=>$userData
+                    ];
+                }   else    {
+                    $result = [
+                        'result'=>'user reg failed'
+                    ];
+                }
+                return new JsonResponse($result);
+        });
+
+
+        $collection->match('/user/{id}', function (Request $request, $id) use ($app) {
+
+                $userID = filter_var($id, FILTER_VALIDATE_INT);
+                if(!$userID) {
+                    throw new \Exception('failed uid');
+                }
+
+                $user = $app['user']->get($userID);
+
+                if($user) {
+                    $result = [
+                        'result'=>'ok',
+                        'user'=>$user
+                    ];
+                }   else {
+                    $result = [
+                        'result'=>'user didn\'t found'
+                    ];
+                }
+                return new JsonResponse($result);
         });
 
         return $collection;
